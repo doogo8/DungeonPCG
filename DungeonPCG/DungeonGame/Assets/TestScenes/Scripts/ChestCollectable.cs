a2640194894f22537d7af452505e8091f66fe0ff
Zachary Petersen <zacharypetersen1@gmail.com>
2015-05-11 10:38:55 -0700

    Basic chest implementation

---
+++ DungeonPCG/DungeonGame/Assets/TestScenes/Scripts/ChestCollectable.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace ProD 
+{
+
+	public class ChestCollectable : MonoBehaviour {
+	
+		public int coinAmount = 5;	//amount of coins gained from this chest
+		Animation chestAnim;
+
+		// Use this for initialization
+		void Start () {
+			chestAnim = gameObject.GetComponent<Animation>();
+			chestAnim.Play("open");
+			chestAnim.Stop();
+		}
+		
+		// Update is called once per frame
+		void Update () {
+		
+		}
+
+		void OnTriggerEnter(Collider other) {
+			if(other.tag == "Player") {
+				other.gameObject.GetComponent<Player>().collectCoins(coinAmount);
+				chestAnim.Play("open");
+				Destroy(this);
+			}
+		}
+	}
+
+}
