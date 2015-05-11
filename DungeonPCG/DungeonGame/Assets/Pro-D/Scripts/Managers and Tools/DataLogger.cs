54c1a20d935bb8808565133d1d03cc32c575e9c7
Cameron Alston <calston@ucsc.edu>
2015-05-11 12:01:38 -0700

    re adding the datalogger

---
+++ DungeonPCG/DungeonGame/Assets/Pro-D/Scripts/Managers and Tools/DataLogger.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DataLogger : MonoBehaviour {
+
+	public GameObject player;
+	public int numJumps;
+	public int totalDamageToEnemies;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//GET PLAYER
+		if(GameObject.Find("adventurer(Clone)") != null){
+			player = GameObject.Find("adventurer(Clone)");
+		}
+		if(GameObject.Find("ai-test-adventurer(Clone)") != null){
+			player = GameObject.Find("ai-test-adventurer(Clone)");
+		}
+		if(GameObject.Find("ai-test-adventurer w_UI(Clone)") != null){
+			player = GameObject.Find("ai-test-adventurer w_UI(Clone)");
+		}
+		//
+
+
+		//Get jumps
+		numJumps = player.GetComponent<ThirdPersonControllerCS>().numJumps;
+		//Get total damage to enemies
+		//totalDamageToEnemies = player.GetComponent<Player>().totalDamageToEnemies;
+	}
+
+	void Exit() {
+
+	}
+}
