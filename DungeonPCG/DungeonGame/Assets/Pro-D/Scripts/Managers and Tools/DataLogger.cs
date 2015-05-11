a3f4fba89313bc936c97476eb2d02006ab02d454
Sam Fields <sam.fields94@gmail.com>
2015-05-11 11:15:06 -0700

    DataLogger in aaron-DungeonGame_Demo; counts jumps only

--- DungeonPCG/DungeonGame/Assets/Pro-D/Scripts/Managers and Tools/DataLogger.cs
+++ DungeonPCG/DungeonGame/Assets/Pro-D/Scripts/Managers and Tools/DataLogger.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class DataLogger : MonoBehaviour {
 
+	public GameObject player;
+	public int numJumps;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +13,24 @@ public class DataLogger : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		//Find the player so we can get information about the player.
+		if(GameObject.Find("adventurer(Clone)") != null){
+			player = GameObject.Find("adventurer(Clone)");
+		}
+		if(GameObject.Find("ai-test-adventurer(Clone)") != null){
+			player = GameObject.Find("ai-test-adventurer(Clone)");
+		}
+		if(GameObject.Find("ai-test-adventurer w_UI(Clone)") != null){
+			player = GameObject.Find("ai-test-adventurer w_UI(Clone)");
+		}
+
+		//Get jumps
+		numJumps = player.GetComponent<ThirdPersonControllerCS>().numJumps;
 	
+	}
+
+	void Exit() {
+
 	}
 }
