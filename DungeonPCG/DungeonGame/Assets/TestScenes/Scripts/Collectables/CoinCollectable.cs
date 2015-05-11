1b77d4e48db474ee8a3f692b4c4f3d120ce29dde
Zachary Petersen <zacharypetersen1@gmail.com>
2015-05-11 11:48:02 -0700

    Created coin prefab

---
+++ DungeonPCG/DungeonGame/Assets/TestScenes/Scripts/Collectables/CoinCollectable.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace ProD 
+{
+
+	public class CoinCollectable : MonoBehaviour {
+	
+		public int coinAmount = 1;	//amount of coins gained from this chest
+
+		//This function is triggered when other collider enters this object's collider
+		void OnTriggerEnter(Collider other) {
+			//Check if other collider belongs to player
+			if(other.tag == "Player") {
+				other.gameObject.GetComponent<Player>().collectCoins(coinAmount);
+				Destroy(this.gameObject);
+			}
+		}
+	}
+
+}
---
+++ DungeonPCG/DungeonGame/Assets/TestScenes/Scripts/Collectables/CollectableRotation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectableRotation : MonoBehaviour {
+
+	public float rotationSpeed = 200;
+
+	// Use this for initialization
+	void Start () {
+		transform.Rotate(Vector3.up * Random.Range(0.0f, 360.0f));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+	}
+}
