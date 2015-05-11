897cf273c2ef4ac20f8ee76b349a87cc53cb913f
Cameron Alston <calston@ucsc.edu>
2015-05-11 12:05:26 -0700

    Fix error, ProD namespace

--- DungeonPCG/DungeonGame/Assets/Pro-D/Scripts/Movement and AI/SpawnMovement.cs
+++ DungeonPCG/DungeonGame/Assets/Pro-D/Scripts/Movement and AI/SpawnMovement.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace ProD
 {
-	public class SpawnMovement : Mtype_Corridorovement
+	public class SpawnMovement : Movement
 	{
 		public override void Setup(WorldMap newWorld)
 		{
