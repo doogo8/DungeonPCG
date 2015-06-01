276f13ac65ef5dac6270627771c7d9b81afdc7cc
Sam Fields <sam.fields94@gmail.com>
2015-06-01 11:58:01 -0700

    Some fog of war stuff added to TestScenes/Resources

---
+++ DungeonPCG/DungeonGame/Assets/Fog_of_War_Sam.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fog_of_War_Sam : MonoBehaviour {
+
+	//If the player gets close to a fog tile, that area
+	//is explored and the tile will disappear now.
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag.Equals ("Player")) {
+			Destroy(this.gameObject);
+		}
+	}
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
